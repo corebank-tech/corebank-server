561b2c6bae184bbf4811633b30284933e905bcbe
astrokan <astrokan@naver.com>
2026-08-01 03:11:55 +0900

    chore(db): 마이그레이션 개편으로 불필요해진 프로젝트 초기 세팅 파일 삭제

--- src/main/resources/db/migration/R__code_transaction_type.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO transaction_type (code, name, sign, created_at, updated_at) VALUES
-  ('DEPOSIT', '입금', 1, '2026-07-28 00:00:00.000000', '2026-07-28 00:00:00.000000'),
-  ('WITHDRAWAL', '출금', -1, '2026-07-28 00:00:00.000000', '2026-07-28 00:00:00.000000')
-ON DUPLICATE KEY UPDATE
-  name = VALUES(name),
-  sign = VALUES(sign),
-  updated_at = VALUES(updated_at);
