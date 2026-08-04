ccb950d822f58147662d505a6da9ac65424b2d0a
vsopsw <vsopsw@gmail.com>
2026-08-04 10:56:08 +0900

    fix(account): 계좌 테이블 컬럼값 변경

--- src/main/resources/db/migration/V202608031651__alter_account_column_defaults.sql
+++ src/main/resources/db/migration/V202608031651__alter_account_column_defaults.sql
@@ -6,4 +6,10 @@ ALTER TABLE account
         COMMENT '행 생성 일시',
 
     MODIFY COLUMN updated_at DATETIME(6) NOT NULL
-        COMMENT '행 최종 수정 일시';
+        COMMENT '행 최종 수정 일시',
+
+    MODIFY COLUMN opened_date DATETIME(6) NOT NULL
+        COMMENT '계좌 개설일',
+
+    MODIFY COLUMN closed_date DATETIME(6) NOT NULL
+        COMMENT '계좌 해지일';
