87b8eb83c6a67972ef35e4b3734c4b565b050f2c
류재성 <homg942@gmail.com>
2026-08-07 16:06:09 +0900

    feat(autotransfer): 조회 포트(AutoTransferQueryPort) 인터페이스 추가

---
+++ src/main/java/com/shinhan/corebank/autotransfer/application/port/out/AutoTransferQueryPort.java
@@ -0,0 +1,10 @@
+package com.shinhan.corebank.autotransfer.application.port.out;
+
+import com.shinhan.corebank.autotransfer.domain.AutoTransfer;
+import com.shinhan.corebank.autotransfer.domain.AutoTransferStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface AutoTransferQueryPort {
+    Page<AutoTransfer> search(Long withdrawalAccountId, AutoTransferStatus status, Pageable pageable);
+}
