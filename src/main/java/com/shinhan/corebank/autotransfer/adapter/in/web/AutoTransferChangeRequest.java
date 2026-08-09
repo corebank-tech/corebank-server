b780a0c1332b9a413efbecd0af9bc13d5cc95736
류재성 <homg942@gmail.com>
2026-08-09 20:51:35 +0900

    feat(autotransfer): 자동이체 변경 요청 DTO(AutoTransferChangeRequest) 추가

---
+++ src/main/java/com/shinhan/corebank/autotransfer/adapter/in/web/AutoTransferChangeRequest.java
@@ -0,0 +1,20 @@
+package com.shinhan.corebank.autotransfer.adapter.in.web;
+
+import com.shinhan.corebank.autotransfer.application.port.in.AutoTransferChangeCommand;
+
+import java.time.LocalDate;
+
+public record AutoTransferChangeRequest(Long amount, Integer cycleMonths, LocalDate endDate, String myPassbookMemo,
+                                        String recipientPassbookMemo, String authToken) {
+    public AutoTransferChangeCommand toCommand(String requestIp) {
+        return AutoTransferChangeCommand.builder()
+                .amount(amount)
+                .cycleMonths(cycleMonths)
+                .endDate(endDate)
+                .myPassbookMemo(myPassbookMemo)
+                .recipientPassbookMemo(recipientPassbookMemo)
+                .authToken(authToken)
+                .requestIp(requestIp)
+                .build();
+    }
+}
