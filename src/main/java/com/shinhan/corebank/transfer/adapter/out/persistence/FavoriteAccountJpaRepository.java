bb29cca3a206ee940446b6d3a3d4fabfa4f574b7
astrokan <astrokan@naver.com>
2026-08-18 20:05:04 +0900

    feat(transfer): FavoriteAccount 영속성 어댑터 추가

---
+++ src/main/java/com/shinhan/corebank/transfer/adapter/out/persistence/FavoriteAccountJpaRepository.java
@@ -0,0 +1,10 @@
+package com.shinhan.corebank.transfer.adapter.out.persistence;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FavoriteAccountJpaRepository extends JpaRepository<FavoriteAccountJpaEntity, Long> {
+    long countByCustomerId(Long customerId);
+    List<FavoriteAccountJpaEntity> findAllByCustomerIdOrderByRegisteredAtDesc(Long customerId);
+}
