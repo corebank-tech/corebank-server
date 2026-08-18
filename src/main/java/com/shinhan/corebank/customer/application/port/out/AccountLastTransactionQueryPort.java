f5784573acb76e170495cea6bfa8e74d39f2abce
astrokan <astrokan@naver.com>
2026-08-18 17:37:07 +0900

    Revert "[feat/customer] 메인 대시보드 접속현황 조회 API"

--- src/main/java/com/shinhan/corebank/customer/application/port/out/AccountLastTransactionQueryPort.java
+++
@@ -1,8 +0,0 @@
-package com.shinhan.corebank.customer.application.port.out;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
-
-public interface AccountLastTransactionQueryPort {
-    Optional<LocalDateTime> findLatestTransactionAt(Long customerId);
-}
