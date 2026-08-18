3143d644387e5f955e28f35b239e5f6eb527f58d
류재성 <homg942@gmail.com>
2026-08-18 16:41:17 +0900

    feat(autotransfer): AutoTransferExecutionJpaRepository에 status별 조회 추가

--- src/main/java/com/shinhan/corebank/autotransfer/adapter/out/persistence/AutoTransferExecutionJpaRepository.java
+++ src/main/java/com/shinhan/corebank/autotransfer/adapter/out/persistence/AutoTransferExecutionJpaRepository.java
@@ -1,6 +1,18 @@
 package com.shinhan.corebank.autotransfer.adapter.out.persistence;
 
+import com.shinhan.corebank.common.domain.ProcessResultStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AutoTransferExecutionJpaRepository extends JpaRepository<AutoTransferExecutionJpaEntity, Long> {
+    @Query("""
+            select e
+            from AutoTransferExecutionJpaEntity e
+            join fetch e.autoTransfer
+            where e.status = :status
+            """)
+    List<AutoTransferExecutionJpaEntity> findAllByStatusWithAutoTransfer(@Param("status")ProcessResultStatus processResultStatus);
 }
