41e8ddaa1dd163d08510544a13496247f6a3dac9
astrokan <astrokan@naver.com>
2026-08-19 12:43:54 +0900

    config: JVM·Hibernate·JDBC 타임존을 UTC에서 KST로 통일

--- src/main/java/com/shinhan/corebank/common/config/JpaAuditingConfig.java
+++ src/main/java/com/shinhan/corebank/common/config/JpaAuditingConfig.java
@@ -1,6 +1,7 @@
 package com.shinhan.corebank.common.config;
 
 import java.time.Clock;
+import java.time.ZoneId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class JpaAuditingConfig {
     @Bean
     public Clock clock() {
-        return Clock.systemUTC();
+        return Clock.system(ZoneId.of("Asia/Seoul"));
     }
 }
